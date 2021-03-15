2885fe10b01c72c18e2fbef37d802c77bce0d5a6
Piotr.Kupis <222488@edu.p.lodz.pl>
2021-03-15 19:13:04 +0100

    testy_v1 added test that checks if number is the last element of a sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,7 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
 
-    SearchResult searchResult;
+    private SearchResult searchResult;
+    private int[] arrayOfOneElements = new int[]{1};
+    private int[] arrayOfFourElements = new int[]{1, 2, 3, 4};
 
     @BeforeEach
     void setUp() throws Exception {
@@ -15,19 +17,25 @@ class BinarySearchTest {
 
     @Test
     void shouldFindInSequence() {
-        searchResult = BinarySearch.search(1, new int[]{1});
+        searchResult = BinarySearch.search(1, arrayOfOneElements);
         assertTrue(searchResult.isFound());
     }
 
     @Test
     void shouldNotFindInSequence() {
-        searchResult = BinarySearch.search(2, new int[]{1});
+        searchResult = BinarySearch.search(2, arrayOfOneElements);
         assertFalse(searchResult.isFound());
     }
 
     @Test
     void shouldBeTheFirstElement() {
-        searchResult = BinarySearch.search(1, new int[]{1,2,3,4});
-        assertEquals(1,searchResult.getPosition());
+        searchResult = BinarySearch.search(1, arrayOfFourElements);
+        assertEquals(1, searchResult.getPosition());
+    }
+
+    @Test
+    void shouldBeTheLastElement() {
+        searchResult = BinarySearch.search(4, arrayOfFourElements);
+        assertEquals(arrayOfFourElements.length, searchResult.getPosition());
     }
 }
