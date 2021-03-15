d2c1953052137307d8123ccaf2f106fa48fbe628
Piotr.Kupis <222488@edu.p.lodz.pl>
2021-03-15 19:19:10 +0100

    testy_v1 added test that checks if number is a median of a sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,7 +9,7 @@ class BinarySearchTest {
 
     private SearchResult searchResult;
     private int[] arrayOfOneElements = new int[]{1};
-    private int[] arrayOfFourElements = new int[]{1, 2, 3, 4};
+    private int[] arrayOfFiveElements = new int[]{1, 2, 3, 4, 5};
 
     @BeforeEach
     void setUp() throws Exception {
@@ -29,13 +29,19 @@ class BinarySearchTest {
 
     @Test
     void shouldBeTheFirstElement() {
-        searchResult = BinarySearch.search(1, arrayOfFourElements);
+        searchResult = BinarySearch.search(1, arrayOfFiveElements);
         assertEquals(1, searchResult.getPosition());
     }
 
     @Test
     void shouldBeTheLastElement() {
-        searchResult = BinarySearch.search(4, arrayOfFourElements);
-        assertEquals(arrayOfFourElements.length, searchResult.getPosition());
+        searchResult = BinarySearch.search(5, arrayOfFiveElements);
+        assertEquals(arrayOfFiveElements.length, searchResult.getPosition());
+    }
+
+    @Test
+    void shouldBeTheMiddleElement() {
+        searchResult = BinarySearch.search(3, arrayOfFiveElements);
+        assertEquals(3, searchResult.getPosition());
     }
 }
